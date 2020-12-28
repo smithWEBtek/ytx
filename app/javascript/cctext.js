6e16962971ec053dfefcca7ced76cf9c68c960f0
smithwebtek <brad@smithwebtek.com>
2020-12-28 17:16:31 -0500

    Draft 1

--- app/javascript/cctext.js
+++ app/javascript/cctext.js
@@ -1,19 +1,34 @@
 $(() => {
   console.log("jquery has entered the building ...");
-  listeners();
+  getVideo()
 });
 
-const listeners = () => {
-  $('.video').on('click', (event) => {
-    // location.href = event.currentTarget.children[0].href
-    $('button.ytp-subtitles-button').trigger();
+const getVideo = () => {
+  $(".video").on("click", (event) => {
+    event.preventDefault();
+    
+    location.href = event.currentTarget.children[0].href
+    $("button.ytp-subtitles-button").click();
+    getCaptions();
+  });
+}
 
-    $.ajax({
-      url: 'https://www.youtube.com/api/timedtext?v=uFBqCPj6n44&asr_langs=de%2Cen%2Ces%2Cfr%2Cit%2Cja%2Cko%2Cnl%2Cpt%2Cru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=en&ip=0.0.0.0&ipbits=0&expire=1609040282&sparams=ip%2Cipbits%2Cexpire%2Cv%2Casr_langs%2Ccaps%2Cexp%2Cxorp%2Cxoaf&signature=DA077C7B51CB2BCB255E5640F11EF08AACBD2B54.057817DCF3C798A66196D66CFE5D080C122B4927&key=yt8&kind=asr&lang=en&fmt=json3&xorb=2&xobt=3&xovt=3',
-      dataType: 'json'
-    }).done(response => {
-      console.log('response: ', response.body)
-      debugger;
-    })
-  })
+const getCaptions = () => {
+  // define regex variable to get captionsUrl
+  let regexp = new RegExp(
+    /playerCaptionsRenderer.*?(youtube.com\/api\/timedtext.*?)"/
+    );
+    
+    // execute regex to get captionsUrl
+    let captionsUrl = regexp.exec(document.body.innerHTML);
+    
+    
+    debugger;
+  $.ajax({
+    url: captionsUrl,
+    dataType: "json",
+  }).done((response) => {
+    console.log("response: ", response);
+    debugger;
+  });
 };
--- app/models/caption.rb
+++ app/models/caption.rb
@@ -3,13 +3,23 @@ class Caption < ApplicationRecord
 
   require 'pry'
   require 'open-uri'
+  
+  # regex to get url
+  # var regexp = new RegExp(/playerCaptionsRenderer.*?(youtube.com\/api\/timedtext.*?)"/);
 
-  def scrape
-    url1 = 'https://www.youtube.com/api/timedtext?v=uFBqCPj6n44&asr_langs=de%2Cen%2Ces%2Cfr%2Cit%2Cja%2Cko%2Cnl%2Cpt%2Cru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=en&ip=0.0.0.0&ipbits=0&expire=1609033526&sparams=ip%2Cipbits%2Cexpire%2Cv%2Casr_langs%2Ccaps%2Cexp%2Cxorp%2Cxoaf&signature=7E4B662E2FF8DB94CB369A664EA1C9DCCCF8CE8F.122814C55AA391F243AF39D5737AF7C509F6436F&key=yt8&kind=asr&lang=en&fmt=json3&xorb=2&xobt=3&xovt=3'
-    url2 = 'https://www.youtube.com/api/timedtext?v=8EQWhQt9OQo&asr_langs=de%2Cen%2Ces%2Cfr%2Cit%2Cja%2Cko%2Cnl%2Cpt%2Cru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=en&ip=0.0.0.0&ipbits=0&expire=1609031060&sparams=ip%2Cipbits%2Cexpire%2Cv%2Casr_langs%2Ccaps%2Cexp%2Cxorp%2Cxoaf&signature=CCC72075691F188F40211AD402B7ECCF2E71C356.9475C661EC490351DB90CE3F9575C554713DCD63&key=yt8&kind=asr&lang=en&fmt=json3&xorb=2&xobt=3&xovt=3'
-    url3 = 'https://www.youtube.com/api/timedtext?v=PKK7HqD8bS8&asr_langs=de%2Cen%2Ces%2Cfr%2Cit%2Cja%2Cko%2Cnl%2Cpt%2Cru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=en&ip=0.0.0.0&ipbits=0&expire=1609047268&sparams=ip%2Cipbits%2Cexpire%2Cv%2Casr_langs%2Ccaps%2Cexp%2Cxorp%2Cxoaf&signature=9A1696DE6AB5D83AE2C6C5B904720476C7F809E0.681E28AB45FAA2AD9BB550B75C632AFAC864D283&key=yt8&kind=asr&lang=en&fmt=json3&xorb=2&xobt=3&xovt=3'
+  # sample caption urls:
+  # url1 = 'https://www.youtube.com/api/timedtext?v=uFBqCPj6n44&asr_langs=de%2Cen%2Ces%2Cfr%2Cit%2Cja%2Cko%2Cnl%2Cpt%2Cru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=en&ip=0.0.0.0&ipbits=0&expire=1609033526&sparams=ip%2Cipbits%2Cexpire%2Cv%2Casr_langs%2Ccaps%2Cexp%2Cxorp%2Cxoaf&signature=7E4B662E2FF8DB94CB369A664EA1C9DCCCF8CE8F.122814C55AA391F243AF39D5737AF7C509F6436F&key=yt8&kind=asr&lang=en&fmt=json3&xorb=2&xobt=3&xovt=3'
+  # url2 = 'https://www.youtube.com/api/timedtext?v=8EQWhQt9OQo&asr_langs=de%2Cen%2Ces%2Cfr%2Cit%2Cja%2Cko%2Cnl%2Cpt%2Cru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=en&ip=0.0.0.0&ipbits=0&expire=1609031060&sparams=ip%2Cipbits%2Cexpire%2Cv%2Casr_langs%2Ccaps%2Cexp%2Cxorp%2Cxoaf&signature=CCC72075691F188F40211AD402B7ECCF2E71C356.9475C661EC490351DB90CE3F9575C554713DCD63&key=yt8&kind=asr&lang=en&fmt=json3&xorb=2&xobt=3&xovt=3'
+  # url3 = 'https://www.youtube.com/api/timedtext?v=PKK7HqD8bS8&asr_langs=de%2Cen%2Ces%2Cfr%2Cit%2Cja%2Cko%2Cnl%2Cpt%2Cru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=en&ip=0.0.0.0&ipbits=0&expire=1609047268&sparams=ip%2Cipbits%2Cexpire%2Cv%2Casr_langs%2Ccaps%2Cexp%2Cxorp%2Cxoaf&signature=9A1696DE6AB5D83AE2C6C5B904720476C7F809E0.681E28AB45FAA2AD9BB550B75C632AFAC864D283&key=yt8&kind=asr&lang=en&fmt=json3&xorb=2&xobt=3&xovt=3'
+  # url4 = 'https://www.youtube.com/api/timedtext?v=oAtazW3rIUg&asr_langs=de%2Cen%2Ces%2Cfr%2Cit%2Cja%2Cko%2Cnl%2Cpt%2Cru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=en&ip=0.0.0.0&ipbits=0&expire=1609113928&sparams=ip%2Cipbits%2Cexpire%2Cv%2Casr_langs%2Ccaps%2Cexp%2Cxorp%2Cxoaf&signature=ED146FC249091EE05854A9D81149874E28D27316.5F9EF3FA0BD8B14911AD4C8D20CE9DD0C43E41A1&key=yt8&kind=asr&lang=en&fmt=json3&xorb=2&xobt=3&xovt=3'
 
-    html = open(url3)
+  def get_caption_url(youtube_url)
+    binding.pry
+
+  end
+
+  def get_cctext(url)
+    html = open(url)
     doc = Nokogiri::HTML(html)
     data = JSON.parse(doc.text)
 
@@ -23,7 +33,12 @@ class Caption < ApplicationRecord
       end
       text.push(line)
     end
-    binding.pry 
+    puts "***********************************************************************************************"
+    puts "***********************************************************************************************"
+    puts text
+    puts "***********************************************************************************************"
+    puts "***********************************************************************************************"
+
   end
 
 end
